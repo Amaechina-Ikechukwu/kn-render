dd1a69ff31a33b32bc08c4d928158a8a4babd9b8
Amaechina Ikechukwu <66841259+Amaechina-Ikechukwu@users.noreply.github.com>
2023-02-12 05:58:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ const createAvatar = require("./profile/createAvatar");
 const createuser = require("./users/createusers");
 const getuser = require("./users/getusers");
 const chatopenai = require("./chats/openai");
-app.use("/.netlify/functions/api`/user", userRouter);
-app.use("/.netlify/functions/api`/getusers", getUser);
-app.use("/.netlify/functions/api`/createavatar", createAvatar);
-app.use("/.netlify/functions/api`/createuser", createuser);
-app.use("/.netlify/functions/api`/getuser", getuser);
-app.use("/.netlify/functions/api`/chatopenai", chatopenai);
+app.use("/user", userRouter);
+app.use("/getusers", getUser);
+app.use("/createavatar", createAvatar);
+app.use("/createuser", createuser);
+app.use("/getuser", getuser);
+app.use("/chatopenai", chatopenai);
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
